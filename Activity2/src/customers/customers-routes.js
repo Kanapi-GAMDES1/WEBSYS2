2e203e0f7fc12163ee0146730b6cacdd480a08c2
DianeC <20237084@s.ubaguio.edu>
2025-09-05 18:40:24 +0800

    Delete Activity2/src directory

--- Activity2/src/customers/customers-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllCustomers, getCustomerById } = require('./customers-controller');
-
-router.get('/', getAllCustomers);
-router.get('/:id', getCustomerById);
-
-module.exports = router;
