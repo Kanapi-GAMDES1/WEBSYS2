b62fba1bf8bbf38066e9ac1bf8beba850daae621
DianeC <20237084@s.ubaguio.edu>
2025-09-05 19:06:28 +0800

    Delete Activity2/src directory

--- Activity2/src/orders/orders-routes.js
+++
@@ -1,8 +0,0 @@
-const express = require('express');
-const router = express.Router();
-const { getAllOrders, getOrderById } = require('./orders-controller');
-
-router.get('/', getAllOrders);
-router.get('/:id', getOrderById);
-
-module.exports = router;
